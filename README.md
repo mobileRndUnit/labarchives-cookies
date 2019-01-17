95be4ca526c66ca8e2ba0c3dd6dd2fe64ae59e6f
Oleksii Kolomatskiy <felexey1@gmail.com>
2019-01-17 16:35:21 +0200

    project was cloned

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# labarchives-cookies
+## labarchives-cookies
+
+Cookie manager for react native.
+
+### Installation
+
+1. `npm install https://gregory.galushka@gitlab.intecracy.com/LabArchives/labarchives-cookies.git --save`
+2. `react-native link labarchives-cookies`
+
+### Usage
+
+```javascript
+import CookieManager from 'labarchives-cookies';
+
+// set a cookie
+CookieManager.set({
+  name: 'myCookie',
+  value: 'myValue',
+  domain: 'some domain',
+  origin: 'some origin',
+  path: '/',
+  version: '1',
+  expiration: '2015-05-30T12:30:00.00-05:00'
+}, (err, res) => {
+  console.log('cookie set!');
+  console.log(err);
+  console.log(res);
+});
+
+```
---
+++ android/src/main/java/com/psykar/cookiemanager/CookieManagerModule.java
@@ -0,0 +1,138 @@
+package com.psykar.cookiemanager;
+
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+
+import com.facebook.react.modules.network.ForwardingCookieHandler;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class CookieManagerModule extends ReactContextBaseJavaModule {
+
+    private ForwardingCookieHandler cookieHandler;
+    private static final String OPTIONS_NAME = "name";
+    private static final String OPTIONS_VALUE = "value";
+    private static final String OPTIONS_DOMAIN = "domain";
+    private static final String OPTIONS_ORIGIN = "origin";
+    private static final String OPTIONS_PATH = "path";
+    private static final String OPTIONS_EXPIRATION = "expiration";
+
+    public CookieManagerModule(ReactApplicationContext context) {
+        super(context);
+        this.cookieHandler = new ForwardingCookieHandler(context);
+    }
+
+    public String getName() {
+        return "RNCookieManagerAndroid";
+    }
+
+    @ReactMethod
+    public void set(ReadableMap options, final Callback callback) throws Exception {
+        String name = null;
+        String value = null;
+        String domain = null;
+        String origin = null;
+        String path = null;
+        String expiration = null;
+        if (options.hasKey(OPTIONS_NAME)) {
+            name = options.getString(OPTIONS_NAME);
+        }
+        if (options.hasKey(OPTIONS_VALUE)) {
+            value = options.getString(OPTIONS_VALUE);
+        }
+        if (options.hasKey(OPTIONS_DOMAIN)) {
+            domain = options.getString(OPTIONS_DOMAIN);
+        }
+        if (options.hasKey(OPTIONS_ORIGIN)) {
+            origin = options.getString(OPTIONS_ORIGIN);
+        }
+        if (options.hasKey(OPTIONS_PATH)) {
+            path = options.getString(OPTIONS_PATH);
+        }
+        if (options.hasKey(OPTIONS_EXPIRATION)) {
+            expiration = options.getString(OPTIONS_EXPIRATION);
+        }
+        Log.d("COOKIE", "SET");
+        CookieSyncManager.createInstance(getCurrentActivity());
+        CookieManager cm = CookieManager.getInstance();
+        cm.setAcceptCookie(true);
+        cm.setCookie("https://" + domain + "/attachments/inline_image/NDg0Mi41fDM3MjUvMzcyNS9Ob3RlYm9vay80MTMxNDU4OTU0fDEyMjkyLjU=?ep_id=MTMuMHwzNzI1LzEwL0VudHJ5UGFydC83OTEzMTA3MjV8MzMuMA%3D%3D&amp;file_name=1497514439338.png", "laapp=" + value);
+        CookieSyncManager.getInstance().sync();
+
+        CookieSyncManager cookieSyncManager = CookieSyncManager.createInstance(getCurrentActivity());
+        CookieSyncManager.getInstance().startSync();
+
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setCookie(origin, name + "=" + value + ";"
+            + OPTIONS_PATH + "=" + path + ";"
+            + OPTIONS_EXPIRATION + "=" + expiration + ";"
+            + OPTIONS_DOMAIN + "=" + domain);
+        cookieSyncManager.sync();
+        Log.d("COOKIE", "SETTED");
+        callback.invoke(null, null);
+
+    }
+
+    @ReactMethod
+    public void setFromResponse(String url, String value, final Callback callback) throws URISyntaxException, IOException {
+        Map headers = new HashMap<String, List<String>>();
+        // Pretend this is a header
+        headers.put("Set-cookie", Collections.singletonList(value));
+        URI uri = new URI(url);
+        this.cookieHandler.put(uri, headers);
+        callback.invoke(null, null);
+    }
+
+    @ReactMethod
+    public void getAll(Callback callback) throws Exception {
+        throw new Exception("Cannot get all cookies on android, try getCookieHeader(url)");
+    }
+
+    @ReactMethod
+    public void get(String url, Callback callback) throws URISyntaxException, IOException {
+        URI uri = new URI(url);
+
+        Map<String, List<String>> cookieMap = this.cookieHandler.get(uri, new HashMap());
+        // If only the variables were public
+        List<String> cookieList = cookieMap.get("Cookie");
+        WritableMap map = Arguments.createMap();
+        if (cookieList != null) {
+            String[] cookies = cookieList.get(0).split(";");
+            for (int i = 0; i < cookies.length; i++) {
+                String[] cookie = cookies[i].split("=");
+                if(cookie.length > 1) {
+                    map.putString(cookie[0].trim(), cookie[1]);
+                }
+            }
+        }
+        callback.invoke(null, map);
+    }
+
+    @ReactMethod
+    public void clearAll(final Callback callback) {
+        final CookieManager cookiesManager = CookieManager.getInstance();
+        cookiesManager.removeAllCookie();
+
+        this.cookieHandler.clearCookies(new Callback() {
+            public void invoke(Object... args) {
+                callback.invoke(null, null);
+            }
+        });
+    }
+}
